1e8093c15d1f1a3e690c00ac7614655b910cdc61
Gustave Legrand <legrandgustave2001@gmail.com>
2024-10-15 15:02:16 +0200

    Error Sentry

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import "./App.css";
 import { CalculatorPage } from "./pages";
 
 const App = () => {
+  const methodDoesNotExist = () => {
+    throw new Error("This is a forced error!"); // Cela provoque une erreur lors de l'exécution
+  };
   return (
+    
     <div className="App">
       <CalculatorPage />
+      <button onClick={() => methodDoesNotExist()}>Break the world</button>;
     </div>
-    <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
   );
 };
 
