a5e61a655e29db8cd1b132fa8cc0fee111e7fad0
Gustave Legrand <legrandgustave2001@gmail.com>
2024-10-15 15:10:26 +0200

    Fix Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -6,24 +6,6 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { theme } from './style/theme';
 
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://7bb9789da81d1dac744901383ed1f844@o4508126818402304.ingest.de.sentry.io/4508126820106320",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
-
-
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <App />
