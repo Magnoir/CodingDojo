a963682185973bf4af13c095a9cc5200b26f0bc7
Gustave Legrand <legrandgustave2001@gmail.com>
2024-10-15 14:27:14 +0200

    Reset Button Add

--- src/pages/Calculator/Calculator.tsx
+++ src/pages/Calculator/Calculator.tsx
@@ -51,6 +51,10 @@ export const CalculatorPage: React.FC<Props> = (props: Props) => {
     });
   };
 
+  const handleReset = () => {
+    setValues({});
+  };  
+
   const result = Object.keys(values).reduce((acc: number, itemName: string) => {
     return acc + values[itemName].kcal;
   }, 0);
@@ -80,6 +84,9 @@ export const CalculatorPage: React.FC<Props> = (props: Props) => {
           <div className={classes.buttonContainer}>
             <Button variant="contained" onClick={sendData(result)}>
               {result} Kcal
+            </Button>
+            <Button variant="outlined" onClick={handleReset} style={{ marginLeft: "10px" }}>
+              Reset
             </Button>
           </div>
         </div>
